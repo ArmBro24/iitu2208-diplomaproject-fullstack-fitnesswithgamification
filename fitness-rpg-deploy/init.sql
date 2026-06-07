3d166a12169913c1c4be7904a946a9723964501b
Arman <160251760+ArmBro24@users.noreply.github.com>
2026-06-08 02:18:27 +0500

    deployment goods
    Штуки для деплоя бэка на AWS

---
+++ fitness-rpg-deploy/init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE reg_db;
+CREATE DATABASE training_db;
+CREATE DATABASE gamification_db;
+CREATE DATABASE challenge_db;
+CREATE DATABASE notification_db;
+CREATE DATABASE payment_db;
+CREATE DATABASE ai_db;
